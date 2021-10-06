0a243b9185d53e39f95549f43869d09d73980e0c
noName <1172903346@qq.com>
2021-10-06 20:09:49 +0800

    new:LeetcodeRecord;

---
+++ No/No430.h
@@ -0,0 +1,45 @@
+//
+// Created by 黄嘉强 on 2021/10/6.
+//
+
+#ifndef LEETCODERECORD_NO430_H
+#define LEETCODERECORD_NO430_H
+
+#include "../bits/stdc++.h"
+
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node *child;
+};
+
+
+class Solution {
+public:
+    Node *flatten(Node *head) {
+        Node* dummy;dummy->next = head;
+
+        Node* pre = nullptr, *mid = head, *temp;
+        while(mid != nullptr){
+            if(mid.child != nullptr){
+                temp = flatten(mid->child);
+                pre = mid;
+                mid = mid->next;
+                pre->next = temp;
+                while(temp->next){
+                    temp = temp->next;
+                }
+                temp->next = mid;
+            }else{
+                mid = mid->next;
+            }
+        }
+        return dummy->next;
+    }
+};
+
+#endif //LEETCODERECORD_NO430_H
